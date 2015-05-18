b262254c4193ad55c680ffa2046fd3cf9eac8551
Brian Seymour <imbrianseymour@gmail.com>
2015-05-18 18:06:58 -0400

    request information output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate hyper;
 
+#[macro_use]
+extern crate log;
+
 use std::io::Write;
 
 use hyper::Server;
@@ -7,12 +10,23 @@ use hyper::server::Request;
 use hyper::server::Response;
 use hyper::net::Fresh;
 
-fn hello(req: Request, res: Response<Fresh>) {
+fn main() {
+    Server::http(bootstrap)
+        .listen("127.0.0.1:8080")
+        .unwrap();
+}
+
+fn bootstrap(req: Request, res: Response<Fresh>) {
+    println!("remote: {}", req.remote_addr);
+    println!("method: {}", req.method);
+    println!("uri: {:?}", req.uri);
+    println!("headers: {}", req.headers);
+    println!("version: {}", req.version);
+
+    //for i in req.body {
+    //    println!("{:?}", i);
+    //}
     let mut res = res.start().unwrap();
     res.write_all(b"Hello World!").unwrap();
     res.end().unwrap();
-}
-
-fn main() {
-    Server::http(hello).listen("127.0.0.1:8080").unwrap();
 }
